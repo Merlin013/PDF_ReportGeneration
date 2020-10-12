ec73cc6a4cd61ca5b14e13e6803612a68e84ee30
Vishal <vishpetkar18@gmail.com>
2020-10-12 07:05:17 +0530

    Added drive name functionality

--- ReportGenerator.py
+++ ReportGenerator.py
@@ -1,9 +1,7 @@
 import PyPDF2 as py
 import re
 import io
-# import tkinter
-# import ghostscript
-# import camelot as cam
+import os
 import tabula
 from tabulate import tabulate
 import xlsxwriter
@@ -17,6 +15,7 @@ def sdname(pfile):
     regex = '(SCOM)'
     drive = '(Logical Disk:)'
     s_name = []
+    drive_name = []
     for i in range(0, num_pages):
         page = pdfRead.getPage(i)
         text += page.extractText()
@@ -29,8 +28,15 @@ def sdname(pfile):
                 print(words1)
                 if re.search('(\d+)',words1[7]):
                     pass
-                drive_name = words1[7]
+                drive_name.append(words1[7])
                 print(drive_name)
+
+        docu = open("Drive names.txt", "a+")
+        # doc.write("List of server names\n")
+        for i in range(len(drive_name)):
+            docu.write(drive_name[i] + "\n")
+
+        docu.close()
     except:
         pass
 
@@ -42,13 +48,42 @@ def sdname(pfile):
             s_name.append(sname[0])
             print(sname[0])
 
-    doc = open("Server names", "a+")
+    doc = open("Server names.txt", "a+")
     # doc.write("List of server names\n")
     for i in range(len(s_name)):
         doc.write(s_name[i] + "\n")
 
     doc.close()
 
+
+def drivenames():
+    dfmain.insert(1, "DriveName", " ")
+    print(dfmain)
+
+    dfr = dfmain.loc[dfmain["Interval"] == "Interval"]
+
+    print(dfr)
+    rowval = list(dfr.index)
+    rowval.append(0)
+    rowval.sort()
+    print(rowval)
+    doc = open("Drive names.txt", "r")
+    lines = doc.readlines()
+    sname = []
+    count = 1
+    for j in lines:
+        sname.append(j)
+    for x in range(len(rowval)):
+        try:
+            while (count < rowval[x + 1]):
+                dfmain.at[count, "DriveName"] = sname[x]
+                count += 1
+        except:
+            if count == rowval[-1]:
+                while (count <= count_rows):
+                    dfmain.at[count, "DriveName"] = sname[-1]
+                    count += 1
+    return
 
 if __name__ == '__main__':
 
@@ -61,7 +96,7 @@ if __name__ == '__main__':
     df = pf.read_csv(output_csv)
     df1 = df[df.columns[0:6]]
     count_rows = df1.shape[0]
-    dfmain = df1.drop(columns= ["Sample Count", "Min Value", "Standard Deviation"])
+    dfmain = df1.drop(columns= ["Sample Count", "Standard Deviation"])
     # dfmain['ServerName'] = None
     dfmain.insert(0, "ServerName", " " )
     print(dfmain)
@@ -74,18 +109,35 @@ if __name__ == '__main__':
     rowval.append(0)
     rowval.sort()
     print(rowval)
-    doc = open("Server names", "r")
+    doc = open("Server names.txt", "r")
     lines = doc.readlines()
     sname = []
+    count = 1
     for j in lines:
         sname.append(j)
     for x in range(len(rowval)):
-        dfmain.at[rowval[x]+1,"ServerName"] = sname[x]
+        try:
+            while (count < rowval[x+1]):
+                dfmain.at[count, "ServerName"] = sname[x]
+                count+=1
+        except:
+            if count == rowval[-1]:
+                while (count <= count_rows):
+                    dfmain.at[count, "ServerName"] = sname[-1]
+                    count += 1
+
+    try:
+        if os.path.exists("Drive names"):
+            drivenames()
+
+    except:
+        pass
+
 
     writer = pf.ExcelWriter('SLAreport.xlsx', engine='openpyxl', mode='w,a')
     dfmain.to_excel(writer, index=False)
     writer.save()
-
+    os.remove(output_csv)
 # global df1, df
 
 # Camelot does not work with this PDF
